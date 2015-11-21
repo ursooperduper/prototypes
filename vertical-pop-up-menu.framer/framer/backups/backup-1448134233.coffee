0b070b31eb5e4f3c8be06c8c2190f75729b970ea
Sarah Kuehnle <sarah@sooperduper.com>
2015-11-21 15:05:49 -0500

    Adds first prototype and Sketch file.

---
+++ vertical-pop-up-menu.framer/framer/backups/backup-1448134233.coffee
@@ -0,0 +1,84 @@
+# Menu prototype
+# Made by Sarah Kuehnle
+# November 21, 2015
+
+# Set device background 
+Framer.Device.background.backgroundColor = "#303030"
+
+# Using an iPhone 5s for demo
+Framer.Device.deviceType = "iPhone-5s-silver"
+
+# This imports all the layers for "menu" into menuLayers
+mLayers = Framer.Importer.load "imported/menu"
+Utils.globalLayers(mLayers)
+
+Framer.Defaults.Animation =
+	curve: "spring(300,30,0)"
+	time: .4
+
+menuVisible = false
+
+btns = [btnDash, btnFeed, btnLock, btnBike, btnUser, btnStats, btnSettings]
+
+for btn in btns
+
+	btn.states.add
+
+		stateVisible:
+			x: btn.x
+			y: btn.y
+			opacity: 1
+			scale: 1
+			rotation: btn.rotationZ + 360
+		
+		stateHidden:
+			x: btnMain.x
+			y: btnMain.y
+			opacity: 0
+			scale: .2
+			rotation: btn.rotationZ
+			
+	btn.states.switchInstant "stateHidden"
+
+chevUp.states.add
+	stateUp:
+		rotation: 0
+		y: 26
+	stateDown: 
+		rotation: 180
+		y: 30
+
+chevUp.states.switchInstant "stateUp"
+
+animationBop = new Animation
+    layer: btnMain
+    properties:
+        scaleX: 1.2
+        scaleY: 1.2
+    curve: "ease-in-out"
+    time: .1
+ 
+animationBopBack = animationBop.reverse()
+animationBop.on(Events.AnimationEnd, animationBopBack.start)
+
+toggleMenu = () ->
+	animationBop.start()
+	if menuVisible == false
+		chevUp.states.switch("stateDown")
+		
+		for btn, i in btns
+			btn.states.switch "stateVisible", delay: .03 * i, time: .1
+		
+		menuVisible = true
+	else
+		chevUp.states.switch("stateUp")
+		
+		for btn, i in btns
+			btn.states.switch "stateHidden", delay: .03 * i, time: .1
+			
+		menuVisible = !menuVisible
+		
+btnMain.on Events.Click, ->
+	toggleMenu()
+	
+
---
+++ vertical-pop-up-menu.framer/imported/menu/layers.json.js
@@ -0,0 +1,242 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["menu/layers.json.js"] = [
+  {
+    "maskFrame" : null,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "id" : "5A37A881-68EE-423B-B023-16954DE8C63F",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "56619F25-FE5D-44F0-81FF-3FF4D6FFC30C",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-chevUp-56619F25-FE5D-44F0-81FF-3FF4D6FFC30C.png",
+              "frame" : {
+                "y" : 1026,
+                "x" : 63,
+                "width" : 41,
+                "height" : 40
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 1026,
+              "x" : 63,
+              "width" : 41,
+              "height" : 40
+            },
+            "name" : "chevUp"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-btnMain-5A37A881-68EE-423B-B023-16954DE8C63F.png",
+          "frame" : {
+            "y" : 1000,
+            "x" : 33,
+            "width" : 100,
+            "height" : 100
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 1000,
+          "x" : 33,
+          "width" : 100,
+          "height" : 100
+        },
+        "name" : "btnMain"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "DEA3FABA-37EE-4207-AA57-F6D71345C9F3",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-btnDash-DEA3FABA-37EE-4207-AA57-F6D71345C9F3.png",
+          "frame" : {
+            "y" : 877,
+            "x" : 38,
+            "width" : 90,
+            "height" : 90
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 877,
+          "x" : 38,
+          "width" : 90,
+          "height" : 90
+        },
+        "name" : "btnDash"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "7C8C32B7-E458-4070-9C46-DD8D653CFFFD",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-btnFeed-7C8C32B7-E458-4070-9C46-DD8D653CFFFD.png",
+          "frame" : {
+            "y" : 758,
+            "x" : 37,
+            "width" : 90,
+            "height" : 90
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 758,
+          "x" : 37,
+          "width" : 90,
+          "height" : 90
+        },
+        "name" : "btnFeed"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "1D0949A5-CD7C-457C-9523-FCE5881CE2A4",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-btnLock-1D0949A5-CD7C-457C-9523-FCE5881CE2A4.png",
+          "frame" : {
+            "y" : 642,
+            "x" : 37,
+            "width" : 90,
+            "height" : 90
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 642,
+          "x" : 37,
+          "width" : 90,
+          "height" : 90
+        },
+        "name" : "btnLock"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "5D8A826E-2DD1-496F-8B4C-6C527CD4A7B0",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-btnBike-5D8A826E-2DD1-496F-8B4C-6C527CD4A7B0.png",
+          "frame" : {
+            "y" : 526,
+            "x" : 37,
+            "width" : 90,
+            "height" : 90
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 526,
+          "x" : 37,
+          "width" : 90,
+          "height" : 90
+        },
+        "name" : "btnBike"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "040DF9E6-AB4B-4EA6-9B42-066AF770E3FA",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-btnUser-040DF9E6-AB4B-4EA6-9B42-066AF770E3FA.png",
+          "frame" : {
+            "y" : 410,
+            "x" : 37,
+            "width" : 90,
+            "height" : 90
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 410,
+          "x" : 37,
+          "width" : 90,
+          "height" : 90
+        },
+        "name" : "btnUser"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "CBD7E3C1-06FC-4B1D-9023-F2E3A0AFC2B4",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-btnStats-CBD7E3C1-06FC-4B1D-9023-F2E3A0AFC2B4.png",
+          "frame" : {
+            "y" : 294,
+            "x" : 37,
+            "width" : 90,
+            "height" : 90
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 294,
+          "x" : 37,
+          "width" : 90,
+          "height" : 90
+        },
+        "name" : "btnStats"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "D89AF0D5-D16E-40D0-991B-CC0CFD012F37",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-btnSettings-D89AF0D5-D16E-40D0-991B-CC0CFD012F37.png",
+          "frame" : {
+            "y" : 178,
+            "x" : 37,
+            "width" : 90,
+            "height" : 90
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 178,
+          "x" : 37,
+          "width" : 90,
+          "height" : 90
+        },
+        "name" : "btnSettings"
+      }
+    ],
+    "id" : "B654EACE-ECE5-4632-8CDB-8EEB91255033",
+    "visible" : true,
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 640,
+      "height" : 1136
+    },
+    "name" : "mainScreen"
+  }
+]
