fdc213acdbf2f0e94a369922dbb8048ff6612d61
Sarah Kuehnle <sarah@sooperduper.com>
2015-11-21 19:25:28 -0500

    Adds wallet-style cards prototype

---
+++ wallet-style-cards.framer/app.coffee
@@ -0,0 +1,77 @@
+# Wallet-Style Cards prototype
+# Made by Sarah Kuehnle
+# November 21, 2015
+
+Framer.Device.background.backgroundColor = "#303030"
+Framer.Device.deviceType = "iPhone-5s-silver"
+
+wLayers = Framer.Importer.load "imported/wallet-style-cards"
+# beta property, be warned
+Utils.globalLayers(wLayers)
+
+Framer.Defaults.Animation =
+	curve: "spring(200,30,0)"
+	time: .4
+
+cards = [cardBlue, cardYellow, cardGreen, cardOrange, cardRed]
+
+# set up card states
+for c, i in cards
+	c.states.add
+		up:
+			x: c.x
+			y: 150 + (i - 1) * 150
+		focus:
+			x: c.x
+			y: 150
+	# set cards in the 'up' position
+	c.states.switchInstant 'up'
+
+# card button events
+cardRed.on Events.Click, ->
+	handleCards(cardRed)
+
+cardOrange.on Events.Click, ->
+	handleCards(cardOrange)
+
+cardGreen.on Events.Click, ->
+	handleCards(cardGreen)
+	
+cardYellow.on Events.Click, ->
+	handleCards(cardYellow)
+
+cardBlue.on Events.Click, ->
+	handleCards(cardBlue)
+
+# object to hold card down animations
+animation = {}
+
+# figure out what to do with cards based on position
+handleCards = (card) ->
+	cardState = card.position
+	if cardState == 'up'
+		card.states.switch 'focus'
+		card.position = 'focus'
+		cardsDown(card)
+	else
+		cardsUp()
+
+# send unfocused cards down
+cardsDown = (card) ->
+	counter = 1
+	startY = 980
+	for c in cards
+		if c != card
+			animation[counter] = new Animation
+				layer: c
+				properties:
+					y: 960 + counter * 20
+			c.position = 'down'
+			animation[counter].start()
+			counter++	
+
+# bring all cards to 'up' position
+cardsUp = () ->
+	for c in cards
+		c.states.switch "up"	
+		c.position = 'up'	
---
+++ wallet-style-cards.framer/imported/wallet-style-cards/layers.json.js
@@ -0,0 +1,388 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["wallet-style-cards/layers.json.js"] = [
+  {
+    "maskFrame" : null,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "id" : "D88F6C05-CE5B-4701-8636-535D69FB2C8E",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "C4BC3BA6-15E5-40C8-8763-40F9EB9D6C4D",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-shareRed-C4BC3BA6-15E5-40C8-8763-40F9EB9D6C4D.png",
+              "frame" : {
+                "y" : 636,
+                "x" : 40,
+                "width" : 52,
+                "height" : 51
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 636,
+              "x" : 40,
+              "width" : 52,
+              "height" : 51
+            },
+            "name" : "shareRed"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "843847E9-9684-41AA-96D5-BC56C931D0AE",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-infoRed-843847E9-9684-41AA-96D5-BC56C931D0AE.png",
+              "frame" : {
+                "y" : 636,
+                "x" : 548,
+                "width" : 52,
+                "height" : 51
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 636,
+              "x" : 548,
+              "width" : 52,
+              "height" : 51
+            },
+            "name" : "infoRed"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-cardRed-D88F6C05-CE5B-4701-8636-535D69FB2C8E.png",
+          "frame" : {
+            "y" : 64,
+            "x" : 20,
+            "width" : 598,
+            "height" : 641
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 64,
+          "x" : 20,
+          "width" : 598,
+          "height" : 641
+        },
+        "name" : "cardRed"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "220303C0-4456-4EB4-AFFA-CF28FEE98F6A",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "950A6C0C-E2DB-4FF5-959E-4147A3E33D66",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-shareRed-950A6C0C-E2DB-4FF5-959E-4147A3E33D66.png",
+              "frame" : {
+                "y" : 636,
+                "x" : 41,
+                "width" : 51,
+                "height" : 51
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 636,
+              "x" : 41,
+              "width" : 51,
+              "height" : 51
+            },
+            "name" : "shareRed"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "31B601C1-43BE-4490-8A92-DF0D0D2292C8",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-infoRed-31B601C1-43BE-4490-8A92-DF0D0D2292C8.png",
+              "frame" : {
+                "y" : 636,
+                "x" : 549,
+                "width" : 51,
+                "height" : 51
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 636,
+              "x" : 549,
+              "width" : 51,
+              "height" : 51
+            },
+            "name" : "infoRed"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-cardOrange-220303C0-4456-4EB4-AFFA-CF28FEE98F6A.png",
+          "frame" : {
+            "y" : 64,
+            "x" : 20,
+            "width" : 599,
+            "height" : 641
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 64,
+          "x" : 20,
+          "width" : 599,
+          "height" : 641
+        },
+        "name" : "cardOrange"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "2D1E7558-C0BF-47B0-9F5F-A436AE6A1665",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "54597969-A0E0-408C-B329-515F6426AC68",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-shareRed-54597969-A0E0-408C-B329-515F6426AC68.png",
+              "frame" : {
+                "y" : 636,
+                "x" : 41,
+                "width" : 51,
+                "height" : 51
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 636,
+              "x" : 41,
+              "width" : 51,
+              "height" : 51
+            },
+            "name" : "shareRed"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "78ED98BF-35C9-4DEA-80A6-B695BB3441A5",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-infoRed-78ED98BF-35C9-4DEA-80A6-B695BB3441A5.png",
+              "frame" : {
+                "y" : 636,
+                "x" : 549,
+                "width" : 51,
+                "height" : 51
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 636,
+              "x" : 549,
+              "width" : 51,
+              "height" : 51
+            },
+            "name" : "infoRed"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-cardGreen-2D1E7558-C0BF-47B0-9F5F-A436AE6A1665.png",
+          "frame" : {
+            "y" : 64,
+            "x" : 20,
+            "width" : 599,
+            "height" : 641
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 64,
+          "x" : 20,
+          "width" : 599,
+          "height" : 641
+        },
+        "name" : "cardGreen"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "5E13F62E-052A-497D-956B-DC64B4F37393",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "5634C46B-5EC3-4B09-AE6E-B684EB8E7F86",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-shareRed-5634C46B-5EC3-4B09-AE6E-B684EB8E7F86.png",
+              "frame" : {
+                "y" : 636,
+                "x" : 41,
+                "width" : 51,
+                "height" : 51
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 636,
+              "x" : 41,
+              "width" : 51,
+              "height" : 51
+            },
+            "name" : "shareRed"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "3B199298-B86F-4F46-B5AB-9C97602705A2",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-infoRed-3B199298-B86F-4F46-B5AB-9C97602705A2.png",
+              "frame" : {
+                "y" : 636,
+                "x" : 549,
+                "width" : 51,
+                "height" : 51
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 636,
+              "x" : 549,
+              "width" : 51,
+              "height" : 51
+            },
+            "name" : "infoRed"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-cardYellow-5E13F62E-052A-497D-956B-DC64B4F37393.png",
+          "frame" : {
+            "y" : 64,
+            "x" : 20,
+            "width" : 599,
+            "height" : 641
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 64,
+          "x" : 20,
+          "width" : 599,
+          "height" : 641
+        },
+        "name" : "cardYellow"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "786DE343-0FDA-4F39-BAF2-0CBC3BA40443",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "A4107CF9-5071-4315-AE13-36E09D418778",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-shareRed-A4107CF9-5071-4315-AE13-36E09D418778.png",
+              "frame" : {
+                "y" : 636,
+                "x" : 41,
+                "width" : 51,
+                "height" : 51
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 636,
+              "x" : 41,
+              "width" : 51,
+              "height" : 51
+            },
+            "name" : "shareRed"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "26017581-8913-48B6-900B-55B70F191A8A",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-infoRed-26017581-8913-48B6-900B-55B70F191A8A.png",
+              "frame" : {
+                "y" : 636,
+                "x" : 549,
+                "width" : 51,
+                "height" : 51
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 636,
+              "x" : 549,
+              "width" : 51,
+              "height" : 51
+            },
+            "name" : "infoRed"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-cardBlue-786DE343-0FDA-4F39-BAF2-0CBC3BA40443.png",
+          "frame" : {
+            "y" : 64,
+            "x" : 20,
+            "width" : 599,
+            "height" : 641
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 64,
+          "x" : 20,
+          "width" : 599,
+          "height" : 641
+        },
+        "name" : "cardBlue"
+      }
+    ],
+    "id" : "4C6FEF42-A09B-4E19-813D-DDA12BF26992",
+    "visible" : true,
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 640,
+      "height" : 1136
+    },
+    "name" : "cardScreen"
+  }
+]
